ffb49124648a30e1d760d10a68a611b3a60c3012
Nathan Richardson <nathanjrichardson3@gmail.com>
2019-10-14 18:23:55 -0500

    First commit of second attempt at jammming project

---
+++ src/Components/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import Playlist from '../Playlist/Playlist';
+import SearchResults from '../SearchResults/SearchResults'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'arist1', album: 'album1', id: 1}, {name: 'name2', artist: 'arist2', album: 'album2', id: 2}, {name: 'name3', artist: 'arist3', album: 'album3', id: 3}],
+
+      playlistName: 'Get Mellow',
+
+      playlistTracks: [{name: 'Here Comes the Sun', artist: 'The Beatles', album: 'Abbey Road', id: 1}, {name: 'St. Percy', artist: 'BrockHampton', album: 'Ginger', id: 2}, {name: 'Nangs', artist: 'Tame Impala', album: 'Currents', id: 3}]
+    }
+  };
+
+  render() {
+    return(
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults} />
+          <Playlist
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+          />
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default App;
